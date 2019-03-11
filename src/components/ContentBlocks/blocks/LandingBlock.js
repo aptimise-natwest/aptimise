1e51dcd5e5a8db952c955bf8de9937a50a8c239e
Simon Richards <s.richards@dewynters.com>
2019-03-11 09:40:05 +0000

    landing latest

--- src/components/ContentBlocks/blocks/LandingBlock.js
+++ src/components/ContentBlocks/blocks/LandingBlock.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Container, Row, Col } from "reactstrap"
 import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import styled from "styled-components"
 import { media } from "utils/Media"
 import Text from "components/shared/Text"
@@ -13,6 +14,8 @@ import imageMobile from "images/backgrounds/landing-home-bg-mobile.jpg"
 const LandingWrapper = styled(Container)`
     max-width: 1500px;
     padding: 0;
+    position: relative;
+    overflow: hidden;
 `
 
 const LandingContent = styled(ContainerMaxWidth)`
@@ -25,15 +28,34 @@ const LandingContent = styled(ContainerMaxWidth)`
     }
 `
 
-const DesktopSvg = styled.svg`
+const DesktopImg = styled(Img)`
     display: none;
+
+    &:after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color:  ${props => props.theme.colors.black};
+        opacity: .3;
+    }
 
     @media ${media.md} {
         display: block;
     }
 `
 
-const MobileSvg = styled.svg`
+const DesktopSvg = styled.svg`
+    position: absolute;
+    bottom: -1%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 102%;
+`
+
+const MobileImg = styled(Img)`
     display: block;
 
     @media ${media.md} {
@@ -70,9 +92,16 @@ const LandingBlock = (props) => {
                 edges {
                     node {
                         id
-                        image {
+                        imageDesktop {
                             childImageSharp  {
                                 fluid(maxWidth: 1400) {
+                                    ...GatsbyImageSharpFluid
+                                }
+                            }
+                        }
+                        imageMobile {
+                            childImageSharp  {
+                                fluid(maxWidth: 768) {
                                     ...GatsbyImageSharpFluid
                                 }
                             }
@@ -92,12 +121,12 @@ const LandingBlock = (props) => {
         ({ node }) => props.id === node.id
     )[0]
 
-    const { title, text } = block.node
+    const { title, text, imageDesktop, imageMobile } = block.node
 
     return (
         <LandingWrapper>
 
-            <DesktopSvg viewBox="0 0 1152 728">  
+            {/* <DesktopSvg viewBox="0 0 1152 728">  
                 <defs>  
                     <clipPath id="desktopClip">  
                         <path d="M1258 328V0L1103.52 1.99892H-16V594L575 728L1258 328Z" />
@@ -105,22 +134,20 @@ const LandingBlock = (props) => {
                 </defs>
                 <image clip-path="url(#desktopClip)" href={image} width="100%" height="100%" />
                 <path d="M1258 328V0L1103.52 1.99892H-16V594L575 728L1258 328Z" fill="black" opacity=".3"/>
-            </DesktopSvg>
+            </DesktopSvg> */}
+
+            <DesktopImg fluid={imageDesktop.childImageSharp.fluid} alt="" />
+
+            <DesktopSvg viewBox="0 0 1315 492">
+                <path class="cls-1" d="M0 266v226h1315V0L610.02 400 0 266z" fill="white" />
+            </DesktopSvg>        
             
             <LandingContent>
                 <Row>
                     <Col md={8} lg={5}>
                         <LandingH1 dangerouslySetInnerHTML={{ __html: title}} />
 
-                        <MobileSvg viewBox="0 0 320 277.7">
-                            <defs>
-                                <clipPath id="mobileClip">
-                                    <path class="st0" d="M0,0l160,34.7L320,0v191.7l-160,86l-160-21L0,0z" />
-                                </clipPath>
-                            </defs>
-                            <image clip-path="url(#mobileClip)" href={imageMobile} width="100%" height="100%" />
-                            <path class="st0" d="M0,0l160,34.7L320,0v191.7l-160,86l-160-21L0,0z" fill="black" opacity=".3" />
-                        </MobileSvg>
+                        <MobileImg fluid={imageMobile.childImageSharp.fluid} alt="" />
 
                         <LandingText
                             dangerouslySetInnerHTML={{ __html: text }}
