1855c2a2d163bda3577a87241838a7070586ceb7
Suri <sk>
2019-04-01 16:41:43 +0100

    Expanded 1st FAQ block;Fixed download pdf button homepage;Added small chat;Added whitepaper thankyou page; Changed Header Natwest link; Updated PDF

--- src/components/ContentBlocks/blocks/LinkBlock.js
+++ src/components/ContentBlocks/blocks/LinkBlock.js
@@ -1,94 +1,99 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
-import styled from "styled-components"
-import { media } from "utils/Media"
-import ContainerMaxWidth from "components/shared/ContainerMaxWidth"
-import Button from "components/shared/Button"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import styled from "styled-components";
+import { media } from "utils/Media";
+import ContainerMaxWidth from "components/shared/ContainerMaxWidth";
+import Button from "components/shared/Button";
 
 const LinkButton = styled(Button)`
-    margin-bottom: .5rem;
-    position: relative;
-    z-index: 1;
-    width: 100%;
+  margin-bottom: 0.5rem;
+  position: relative;
+  z-index: 1;
+  width: 100%;
 
-    @media ${media.md} {
-         width: auto;
-        margin-right: .5rem;
+  @media ${media.md} {
+    width: auto;
+    margin-right: 0.5rem;
+  }
+
+  &.internalLink {
+    color: ${props => props.theme.colors.blackOff};
+    background-color: ${props => props.theme.colors.grey};
+    &:hover {
+      color: ${props => props.theme.colors.white};
+      background-color: ${props => props.theme.colors.purpleDark};
     }
+  }
+`;
 
-
-    &.internalLink {
-        color: ${props => props.theme.colors.blackOff};
-        background-color: ${ props => props.theme.colors.grey};
-        &:hover {
-            color: ${props => props.theme.colors.white};
-            background-color: ${props => props.theme.colors.purpleDark };
-        }
-    }
-`
-
-const LinkBlock = (props) => {
-
-    const data = useStaticQuery(graphql`
-        query {
-            allContentBlocksJson {
-                edges {
-                    node {
-                        id
-                        links {
-                            link
-                            linkText
-                        }
-                    }
-                }
+const LinkBlock = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentBlocksJson {
+        edges {
+          node {
+            id
+            links {
+              link
+              linkText
+              download
             }
+          }
         }
-    `)
+      }
+    }
+  `);
 
-    // Retrieve block
-    const block = data.allContentBlocksJson.edges.filter(
-        ({ node }) => props.id === node.id
-    )[0]
+  // Retrieve block
+  const block = data.allContentBlocksJson.edges.filter(
+    ({ node }) => props.id === node.id
+  )[0];
+  const links = block.node.links.map((link, i) => {
+    if (link.link.charAt(0) === "/") {
+      // Switch to gatsby Link if internal
+      let linkButton;
+      if (!link.download) {
+        linkButton = (
+          <LinkButton key={i} as={Link} className="internalLink" to={link.link}>
+            {link.linkText}
+          </LinkButton>
+        );
+      } else {
+        linkButton = (
+          <LinkButton
+            key={i}
+            className="internalLink"
+            as="a"
+            href={link.link}
+            download
+          >
+            {link.linkText}
+          </LinkButton>
+        );
+      }
+      return linkButton;
+    } else {
+      return (
+        <LinkButton
+          key={i}
+          href={link.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {link.linkText}
+        </LinkButton>
+      );
+    }
+  });
 
-    const links = block.node.links.map( (link, i) => {
-
-        if (link.link.charAt(0) === "/") {
-            // Switch to gatsby Link if internal
-            return (
-                <LinkButton
-                    key={i}
-                    as={Link}
-                    className="internalLink"
-                    to={link.link}
-                >
-                    {link.linkText}
-                </LinkButton>
-            )
-        } else {
-            return (
-                <LinkButton
-                    key={i}
-                    href={link.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {link.linkText}
-                </LinkButton>
-            )
-        }
-
-    })
-
-    return (
-        <ContainerMaxWidth className="py-3 py-lg-4">
-            { links }
-        </ContainerMaxWidth>
-    )
-}
+  return (
+    <ContainerMaxWidth className="py-3 py-lg-4">{links}</ContainerMaxWidth>
+  );
+};
 
 LinkBlock.propTypes = {
-    id: PropTypes.node.isRequired,
-}
+  id: PropTypes.node.isRequired
+};
 
-export default LinkBlock
+export default LinkBlock;
--- src/templates/DefaultTemplate.js
+++ src/templates/DefaultTemplate.js
@@ -1,23 +1,40 @@
-import React, { Component } from "react"
-import Layout from 'components/Layout/Layout'
-import ContentBlocks from 'components/ContentBlocks/ContentBlocks'
-import ContainerMaxWidth from "components/shared/ContainerMaxWidth"
+import React, { Component } from "react";
+import Layout from "components/Layout/Layout";
+import ContentBlocks from "components/ContentBlocks/ContentBlocks";
+import ContainerMaxWidth from "components/shared/ContainerMaxWidth";
 
 class DefaultTemplate extends Component {
-    render() {
-        const data = this.props.data
+  constructor(props) {
+    super(props);
+    this.createChat = this.createChat.bind(this);
+  }
 
-        return (
-            <Layout data={data}>
-                {data.pagesJson.h1 !== null &&
-                    <ContainerMaxWidth >
-                        <h1 className="pt-4">{data.pagesJson.title}</h1>
-                    </ContainerMaxWidth>
-                }
-                <ContentBlocks data={data} />
-            </Layout>
-        )
-    }
+  createChat() {
+    const src = "https://embed.small.chat/TENLCNU93GHCB0RK08.js";
+    const s = document.createElement("script");
+    s.setAttribute("src", src);
+    s.setAttribute("async", true);
+    s.setAttribute("id", "chatScript");
+    document.body.appendChild(s);
+  }
+
+  componentDidMount() {
+    this.createChat();
+  }
+  render() {
+    const data = this.props.data;
+
+    return (
+      <Layout data={data}>
+        {data.pagesJson.h1 !== null && (
+          <ContainerMaxWidth>
+            <h1 className="pt-4">{data.pagesJson.title}</h1>
+          </ContainerMaxWidth>
+        )}
+        <ContentBlocks data={data} />
+      </Layout>
+    );
+  }
 }
 
-export default DefaultTemplate
+export default DefaultTemplate;
